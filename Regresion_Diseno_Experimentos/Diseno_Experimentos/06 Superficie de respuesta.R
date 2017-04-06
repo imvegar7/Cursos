602ff6d36ca99bb16640beefa30c0b8c3b30937d
fhernanb <fhernanb@unal.edu.co>
2017-04-06 08:28:32 -0500

    se organizaron

--- Regresion_Diseno_Experimentos/Diseno_Experimentos/06 Superficie de respuesta.R
+++ Regresion_Diseno_Experimentos/Diseno_Experimentos/06 Superficie de respuesta.R
@@ -1,22 +1,34 @@
 
 # For example 1 -----------------------------------------------------------
-fun <- function(x1, x2) 35.5 + 10.5 * x1 + 5.5 * x2
+fun <- function(x1, x2) 35.5 + (21/2) * x1 + (11*2) * x2 + (1/2) * x1 * x2
 fun <- Vectorize(fun)
 x1 <- seq(from=-1, to=1, length.out=7)
 x2 <- seq(from=-1, to=1, length.out=7)
 y <- outer(x1, x2, fun)
 
+# Superficie en 3d
 persp(x1, x2, y, theta=30, phi=30,
       ticktype = "detailed", nticks=3,
       col='springgreen1', border='blue',
       xlab='Factor A', ylab='Factor B',
       zlab='Response')
 
+# Grafico de contornos
+contour(x=x1, y=x2, z=y, nlevels=50,
+        col='salmon', lwd=1, lty='solid',
+        xlab=expression(x[1]), ylab=expression(x[2]))
+
+# Grafico de niveles
+filled.contour(x=x1, y=x2, z=y, nlevels=20,
+               xlab=expression(x[1]), ylab=expression(x[2]),
+               color=topo.colors)
+
+
 # For example 2 -----------------------------------------------------------
-fun <- function(x1, x2) 30.5 + 1*x1 - 9*x2 - (29/2)*x1*x2
+fun <- function(x1, x2) 30.5 + (1/2) * x1 - (9/2) * x2 - (29/2)*x1*x2
 fun <- Vectorize(fun)
-x1 <- seq(from=-1, to=1, length.out=7)
-x2 <- seq(from=-1, to=1, length.out=7)
+x1 <- seq(from=-1, to=1, length.out=15)
+x2 <- seq(from=-1, to=1, length.out=15)
 y <- outer(x1, x2, fun)
 
 persp(x1, x2, y, theta=20, phi=30,
@@ -25,7 +37,18 @@ persp(x1, x2, y, theta=20, phi=30,
       xlab='Factor A', ylab='Factor B',
       zlab='Response')
 
-# Using rgl package
+# Grafico de contornos
+contour(x=x1, y=x2, z=y, nlevels=50,
+        col='blue', lwd=3, lty='solid',
+        xlab=expression(x[1]), ylab=expression(x[2]))
+
+# Grafico de niveles
+filled.contour(x=x1, y=x2, z=y, nlevels=20,
+               xlab=expression(x[1]), ylab=expression(x[2]),
+               color=heat.colors)
+
+
+# Pretty response surface using rgl package -------------------------------
 library(rgl)
 
 fun <- function(x1, x2) 30.5 + 1*x1 - 9*x2 - (29/2)*x1*x2
