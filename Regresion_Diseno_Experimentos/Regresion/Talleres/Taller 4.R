61696c25bd27f62b123c0a096d4aa80e48136a23
fhernanb <fhernanb@unal.edu.co>
2017-04-25 09:24:33 -0500

    mejoras

--- Regresion_Diseno_Experimentos/Regresion/Talleres/Taller 4.R
+++ Regresion_Diseno_Experimentos/Regresion/Talleres/Taller 4.R
@@ -2,16 +2,43 @@
 datos <- read.table(file='http://tinyurl.com/hwhb769', header=T)
 
 # Punto 1
+summary(datos[, c('mt2', 'alcobas')])
+
+# Punto 2
 with(datos, symbols(x=mt2, y=precio,
                     squares=alcobas,
                     las=1, inches=0.4,
                     fg='dodgerblue4', bg='pink',
                     main='Lado = N° alcobas'))
+# Punto 3
+obs.rara <- which.max(datos$alcobas)
+obs.rara
+datos[obs.rara, ]
 
-# Punto 2
-mod <- lm(precio ~ mt2 + alcobas, data=datos)
+# Punto 4
+datos2 <- datos[-obs.rara, ]
+with(datos2, symbols(x=mt2, y=precio,
+                     squares=alcobas,
+                     las=1, inches=0.2,
+                     fg='dodgerblue4',
+                     main='Lado = N° alcobas'))
+
+# Punto 5
+mod <- lm(precio ~ mt2 + alcobas, data=datos2)
 summary(mod)
 
-# Punto 3
-# Se escribe usando los resultados del summary
+# Punto 6
+mu.est <- function(area, habit) sum(coef(mod) * c(1, area, habit))
+mu.est <- Vectorize(mu.est) # Esto para vectorizar la funcion
+
+Areas <- seq(from=26, to=500, length.out=100)
+Habit <- 1:5
+y <- outer(Areas, Habit, mu.est)
+
+# Grafico de contornos
+contour(x=Areas, y=Habit, z=y, nlevels=20,
+        lwd=1, lty='solid',
+        xlab='Area', ylab='No alcobas')
+
+
 
